0b8076b306b625e40d183e048fcf63c5b514dd54
Auriel James <aurieljames11@gmail.com>
2023-08-16 02:16:32 +0800

    issue/#1 Installed tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
-<head></head>
-<body>
-    <h1>Hi this is my instagram website</h1>
+<head>
+<title>Laravel Project</title>
+@vite('resources/css/app.css')
+</head>
+<body >
+<h1 class="text-3xl font-bold underline bg-red-700">
+    Hello world!
+  </h1>
 </body>
 </html>
